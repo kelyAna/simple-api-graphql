2a27f48bfc5bbc5a13d52b5bf306c24160bbd46f
kelyAna <kely.lopes@vtex.com.br>
2023-07-18 09:08:40 -0300

    feat: add schema

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata'
+import path from 'node:path'
+
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+import { AppointementsResolver } from './resolvers/appointments-resolver'
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [
+      AppointementsResolver,
+    ],
+    emitSchemaFile: path.resolve(__dirname, 'schema.gql')
+  })
+  const server = new ApolloServer({
+    schema
+  })
+
+  const { url } = await server.listen()
+
+  console.log(`HTTP server running on ${url}`)
+
+}
+
+bootstrap()
